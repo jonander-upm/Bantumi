912bbe371eacbae438fcf348dd6ab80eaa89bc4d
jonander-upm <jonander.gainzarain@alumnos.upm.es>
2022-10-25 13:54:51 +0200

    Added restart dialog

---
+++ app/src/main/java/es/upm/miw/bantumi/RestartAlertDialog.java
@@ -0,0 +1,43 @@
+package es.upm.miw.bantumi;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class RestartAlertDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+	public AppCompatDialog onCreateDialog(Bundle savedInstanceState) {
+		final MainActivity main = (MainActivity) getActivity();
+
+        assert main != null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(main);
+        builder
+                .setTitle(R.string.txtDialogoReinicioTitulo)
+                .setMessage(R.string.txtDialogoReinicioPregunta)
+                .setPositiveButton(
+                        getString(R.string.txtDialogoReinicioAfirmativo),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                main.juegoBantumi.inicializar(JuegoBantumi.Turno.turnoJ1);
+                            }
+                        }
+                )
+                .setNegativeButton(
+                        getString(R.string.txtDialogoReinicioNegativo),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                main.finish();
+                            }
+                        }
+                );
+
+		return builder.create();
+	}
+}
